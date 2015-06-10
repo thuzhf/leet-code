ad4acfc8db828316b75d11dae2589fe30620caca
thuzhf <thuzhf@gmail.com>
2015-06-10 13:56:37 +0800

    fix a bug: get wrong if *root* is nullptr

--- 112_path-sum/path-sum.hpp
+++ 112_path-sum/path-sum.hpp
@@ -18,6 +18,8 @@ class Solution {
 
 	public:
 		bool hasPathSum(TreeNode* root, int sum) {
+			if (!root)
+				return false;
 			TN_.push_back(std::forward_as_tuple(root, 0));
 			int current_sum;
 			while (!TN_.empty()) {
@@ -32,38 +34,6 @@ class Solution {
 					TN_.push_back(std::forward_as_tuple(node->left, current_sum));
 				if (node->right)
 					TN_.push_back(std::forward_as_tuple(node->right, current_sum));
-			}
-			return false;
-		}
-
-		bool hasPathSum2(TreeNode* root, int sum) {
-			if (!root || root->val > sum)
-				return false;
-			else if (root->val == sum)
-				return true;
-
-			TN_.push_back(std::forward_as_tuple(root, root->val));
-			int left_part_sum = 0;
-			int right_part_sum = 0;
-			while (!TN_.empty()) {
-				auto current = TN_.back();
-				TN_.pop_back();
-				auto&& node = std::get<0>(current);
-				auto&& part_sum = std::get<1>(current);
-				if (node->left) {
-					left_part_sum = node->left->val + part_sum;
-					if (left_part_sum == sum)
-						return true;
-					else if (left_part_sum < sum)
-						TN_.push_back(std::forward_as_tuple(node->left, left_part_sum));
-				}
-				if (node->right) {
-					right_part_sum = node->right->val + part_sum;
-					if (right_part_sum == sum)
-						return true;
-					else if (left_part_sum < sum)
-						TN_.push_back(std::forward_as_tuple(node->right, right_part_sum));
-				}
 			}
 			return false;
 		}
