7fcd39529579a2b28df0a8e89d264ff1453c492a
thuzhf <thuzhf@gmail.com>
2015-06-12 12:52:34 +0800

    fix a bug: negative should output false

--- 009_palindrome-number/palindrome-number.hpp
+++ 009_palindrome-number/palindrome-number.hpp
@@ -2,15 +2,13 @@
 #define PALINDROME_HPP_
 
 #include <vector>
-#include <climits>
 
 class Solution {
 	public:
 		bool isPalindrome(int x) {
+			if (x < 0)  return false;
 			if (x / 10 == 0)  return true;
 			if (x % 10 == 0)  return false;
-			if (x == INT_MIN) return false;
-			if (x < 0)  x = -x;
 			std::vector<int> digits;
 			while (x) {
 				digits.push_back(x % 10);
