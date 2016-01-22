35d98f5a4af9d423a175d10c4a23f0ac2f82d99c
thuzhf <thuzhf@gmail.com>
2016-01-22 12:29:22 +0800

    little change

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 # @Email:  thuzhf@gmail.com
 # @Date:   2016-01-22 11:55:51
 # @Last Modified by:   zhangfang
-# @Last Modified time: 2016-01-22 12:19:53
+# @Last Modified time: 2016-01-22 12:28:10
 
 from __future__ import print_function,division,unicode_literals,absolute_import
 import sys,os,re,json,gzip,math,time,datetime,functools,contextlib,itertools
@@ -23,8 +23,10 @@ def main():
     if len(sys.argv) != 2:
         print('Usage: ./setup.py dir_name')
     dir_name = sys.argv[1]
+    question_name = dir_name.split('_')[-1]
     sp.call(['mkdir', dir_name])
     sp.call(['cp', 'main.cc', 'xxx.hh', 'Makefile', dir_name])
+    sp.call(['mv', '{:s}/xxx.hh'.format(dir_name), '{:s}/{:s}.hh'.format(dir_name, question_name)])
     pass
 
 if __name__ == '__main__':
