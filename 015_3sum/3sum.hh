1915455474bea1e6c382439850b0bc92215f81d5
thuzhf <thuzhf@gmail.com>
2016-01-22 13:58:56 +0800

    fix a bug

--- 015_3sum/3sum.hh
+++ 015_3sum/3sum.hh
@@ -2,7 +2,7 @@
 * @Author: zhangfang
 * @Date:   2016-01-22 12:31:27
 * @Last Modified by:   zhangfang
-* @Last Modified time: 2016-01-22 13:16:51
+* @Last Modified time: 2016-01-22 13:57:54
 */
 #pragma once
 #include <iostream>
@@ -12,20 +12,25 @@
 class Solution {
 private:
     int last_one_[3];
+
 public:
     std::vector<std::vector<int> > threeSum(std::vector<int>& nums) {
         std::sort(nums.begin(), nums.end());
         int nums_size = nums.size();
         std::vector<std::vector<int> > ans;
+
         for (int i = 0; i < nums_size; ++ i) {
-            if (i > 0 && last_one_[0] == nums[i])
+            if (i > 0 && last_one_[0] == nums[i]) {
                 continue;
+            }
             for (int j = i + 1; j < nums_size; ++ j) {
-                if (j > 1 && last_one_[1] == nums[j])
+                if (j > i + 1 && last_one_[1] == nums[j]) {
                     continue;
+                }
                 for (int k = j + 1; k < nums_size; ++ k) {
-                    if (k > 2 && last_one_[2] == nums[k])
+                    if (k > j + 1 && last_one_[2] == nums[k]) {
                         continue;
+                    }
                     if (nums[i] + nums[j] + nums[k] == 0) {
                         ans.push_back(std::vector<int>({nums[i], nums[j], nums[k]}));
                     }
