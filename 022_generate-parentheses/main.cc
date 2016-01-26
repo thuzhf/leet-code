2d2bc337bded1351e9c0a7131e96c82fc14c7003
thuzhf <thuzhf@gmail.com>
2016-01-26 17:34:11 +0800

    calculate the num of all combinations for small n

--- 022_generate-parentheses/main.cc
+++ 022_generate-parentheses/main.cc
@@ -1,5 +1,5 @@
 // FIXME
-#include "xxx.hh"
+#include "generate-parentheses.hh"
 
 #include <iostream>
 #include <vector>
@@ -10,6 +10,14 @@ using namespace std;
 int main()
 {
 	Solution sol;
+    vector<int> ans;
+    for (int i = 1; i <= 15; ++i) {
+        std::vector<std::string> str = sol.generateParenthesis(i);
+        ans.push_back(str.size());
+    }
+    for (auto i = 0; i < ans.size(); ++i) {
+        cout << ans[i] << endl;
+    }
 
 	return 0;
 }
