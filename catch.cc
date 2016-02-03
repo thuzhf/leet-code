26aa643c5f55b4bafcf476442d4bfab04f52c367
thuzhf <thuzhf@gmail.com>
2016-02-03 21:46:22 +0800

    use Catch to perform unit testing

---
+++ catch.cc
@@ -0,0 +1,10 @@
+/* 
+* @Author: zhangfang
+* @Email:  thuzhf@gmail.com
+* @Date:   2016-01-31 22:48:13
+* @Last Modified by:   zhangfang
+* @Last Modified time: 2016-01-31 22:53:37
+*/
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hh"
